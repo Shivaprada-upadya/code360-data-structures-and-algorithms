40c3a10ab8700d2c7257a796d293d51549bad128
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-30 22:33:08 +0530

    Add CAlculate Square Of a Number in BAsic Programming Folder

---
+++ basic-programming/calculate-square-of-a-number.java
@@ -0,0 +1,14 @@
+//Problem link: https://www.naukri.com/code360/problem-of-the-day/easy?date=2025-01-03
+
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static int calculateSquare(int num) 
+    {
+        //    Write your code here.
+        return num*num;
+    }
+}
